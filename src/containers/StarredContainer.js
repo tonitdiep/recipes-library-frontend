e98af2efa015c2cde84a24714df6cda780bbe98f
tonitdiep <adiep123@gmail.com>
2021-07-08 00:17:08 -0400

    update

--- src/containers/StarredContainer.js
+++ src/containers/StarredContainer.js
@@ -4,26 +4,29 @@ import RecipeList from '../components/RecipeList'
 import {Link} from 'react-router-dom'
 const StarredContainer = (props) => {
     console.log('inside starred container', props)
-//    const recipes = props.recipes.map((recipe) => {
-//   return <li key={recipe.id}></li>})
-//   let recipe = props.recipes.filter(recipe => recipe.id == props.match.params.id)[0]
+    debugger
+    let recipe = props.recipes.filter(recipe => recipe.id == props.match.params.id)[0]
+    // let recipes = props.recipes.filter(recipe => recipe.starred === true)
 
-// let recipe = props.recipes.filter(recipe => recipe.id == props.match.params.id)[0]
     return (
+
         <div>
             <h2>StarredContainer</h2>
-            {/* <RecipeList/> */}
-            {/* <ul> */}
-            {/* {recipe = props.recipes.filter(recipe => recipe.id == props.match.params.id)[0]} */}
-                {/* {recipe.starred} */}
-            {/* {props.recipes && props.recipes.starred.filter(recipe => */}
-                {/* <li key={recipe.id}> */}
-                    {/* <Link to={`/recipes/${recipe.id}`}> {recipe.name} </Link> */}
-                {/* </li> */}
-            {/* )} */}
-            {/* </ul> */}
+            Starred Recipe: {recipe.starred ? "Checked Recipe" : "Not A Fav"}
+            
+             {/* {<ul>
+
+                {props.recipes && props.recipes.filter(recipe => 
+                    <li key={recipe.id}>
+                        <Link to={`/recipes/${recipe.id}`}> {recipe.name}</Link>
+                    </li>
+        
+                )}
+            </ul>}  */}
+            
         </div>
     )
 }
 
 export default StarredContainer
+
